22ab3a932955761f40b2d9e42aed6efa04a0d742
japan00x <137403487+japan00x@users.noreply.github.com>
2025-04-05 05:15:44 +0900

    Update main.cpp

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,3 +1,9 @@
+/*
+    PSP_HttpsBrowser.cpp
+    PSP 用 HTTPS ブラウザ（例：example.com へ GET リクエストを送信）
+    TLS ライブラリは wolfSSL を利用（mbedtls は使用しない）
+*/
+
 #include <pspkernel.h>
 #include <pspdebug.h>
 #include <pspnet.h>
@@ -5,18 +11,15 @@
 #include <pspnet_resolver.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-// mbedtls ライブラリのヘッダ
-#include "mbedtls/net_sockets.h"
-#include "mbedtls/ssl.h"
-#include "mbedtls/ssl_internal.h"
-#include "mbedtls/error.h"
-#include "mbedtls/debug.h"
-#include "mbedtls/ctr_drbg.h"
-#include "mbedtls/entropy.h"
+// wolfSSL ヘッダ
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
 
-// モジュール情報
+// PSP モジュール情報
 PSP_MODULE_INFO("PSP_HttpsBrowser", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
 
 // 終了用コールバック
 int exit_callback(int arg1, int arg2, void* common) {
@@ -24,6 +27,7 @@ int exit_callback(int arg1, int arg2, void* common) {
     return 0;
 }
 
+// コールバックスレッド
 int callbackThread(SceSize args, void* argp) {
     int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
     sceKernelRegisterExitCallback(cbid);
@@ -31,6 +35,7 @@ int callbackThread(SceSize args, void* argp) {
     return 0;
 }
 
+// コールバックセットアップ
 void setupCallbacks() {
     int thid = sceKernelCreateThread("update_thread", callbackThread, 0x11, 0xFA0, 0, NULL);
     if(thid >= 0){
@@ -38,148 +43,187 @@ void setupCallbacks() {
     }
 }
 
-// HTTPS 通信サンプル（example.com へ GET リクエストを送信）
-int https_request()
-{
-    const char *server_name = "example.com";
-    const char *server_port = "443";
-    const char *GET_request = "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n";
-    int ret;
-
-    // mbedtls用各種コンテキスト初期化
-    mbedtls_net_context server_fd;
-    mbedtls_ssl_context ssl;
-    mbedtls_ssl_config conf;
-    mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_entropy_context entropy;
-    
-    mbedtls_net_init(&server_fd);
-    mbedtls_ssl_init(&ssl);
-    mbedtls_ssl_config_init(&conf);
-    mbedtls_ctr_drbg_init(&ctr_drbg);
-    mbedtls_entropy_init(&entropy);
-
-    // ランダム生成器の初期化
-    const char *pers = "psp_https_browser";
-    if((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-                                      (const unsigned char *) pers, strlen(pers))) != 0) {
-        pspDebugScreenPrintf("mbedtls_ctr_drbg_seed returned -0x%04X\n", -ret);
-        goto exit;
-    }
-
-    // サーバへ TCP 接続
-    pspDebugScreenPrintf("Connecting to %s:%s...\n", server_name, server_port);
-    if((ret = mbedtls_net_connect(&server_fd, server_name, server_port, MBEDTLS_NET_PROTO_TCP)) != 0) {
-        pspDebugScreenPrintf("mbedtls_net_connect returned -0x%04X\n", -ret);
-        goto exit;
-    }
-
-    // SSL設定の初期化
-    if((ret = mbedtls_ssl_config_defaults(&conf,
-                    MBEDTLS_SSL_IS_CLIENT,
-                    MBEDTLS_SSL_TRANSPORT_STREAM,
-                    MBEDTLS_SSL_PRESET_DEFAULT)) != 0) {
-        pspDebugScreenPrintf("mbedtls_ssl_config_defaults returned -0x%04X\n", -ret);
-        goto exit;
-    }
-
-    // ここで証明書検証を無効化（古い証明書による接続障害回避のため）
-    mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_NONE);
-    mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
-    // 必要に応じてデバッグ出力も設定可能
-    // mbedtls_ssl_conf_dbg(&conf, mbedtls_debug, NULL);
-
-    if((ret = mbedtls_ssl_setup(&ssl, &conf)) != 0) {
-        pspDebugScreenPrintf("mbedtls_ssl_setup returned -0x%04X\n", -ret);
-        goto exit;
-    }
-
-    // サーバ名設定（SNI 対応）
-    if((ret = mbedtls_ssl_set_hostname(&ssl, server_name)) != 0) {
-        pspDebugScreenPrintf("mbedtls_ssl_set_hostname returned -0x%04X\n", -ret);
-        goto exit;
-    }
-
-    // BIO を設定してネットソケットと接続
-    mbedtls_ssl_set_bio(&ssl, &server_fd, mbedtls_net_send, mbedtls_net_recv, NULL);
-
-    // SSL ハンドシェイク
-    pspDebugScreenPrintf("Performing the SSL/TLS handshake...\n");
-    while((ret = mbedtls_ssl_handshake(&ssl)) != 0) {
-        if(ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
-            pspDebugScreenPrintf("mbedtls_ssl_handshake returned -0x%04X\n", -ret);
-            goto exit;
-        }
-    }
-    pspDebugScreenPrintf("Handshake successful!\n");
-
-    // GETリクエスト送信
-    pspDebugScreenPrintf("Sending GET request...\n");
-    ret = mbedtls_ssl_write(&ssl, (const unsigned char *) GET_request, strlen(GET_request));
-    if(ret <= 0) {
-        pspDebugScreenPrintf("mbedtls_ssl_write returned -0x%04X\n", -ret);
-        goto exit;
-    }
-
-    // レスポンスを受信して画面に表示
-    pspDebugScreenPrintf("Receiving response...\n");
-    unsigned char buf[1024];
-    do {
-        memset(buf, 0, sizeof(buf));
-        ret = mbedtls_ssl_read(&ssl, buf, sizeof(buf)-1);
-        if(ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE)
-            continue;
-        if(ret <= 0)
-            break;
-        buf[ret] = '\0';
-        pspDebugScreenPrintf("%s", buf);
-    } while(1);
-
-    pspDebugScreenPrintf("\nConnection closed.\n");
-
-exit:
-    mbedtls_ssl_close_notify(&ssl);
-    mbedtls_net_free(&server_fd);
-    mbedtls_ssl_free(&ssl);
-    mbedtls_ssl_config_free(&conf);
-    mbedtls_ctr_drbg_free(&ctr_drbg);
-    mbedtls_entropy_free(&entropy);
-    return ret;
-}
-
-int main() {
-    setupCallbacks();
-    pspDebugScreenInit();
-    pspDebugScreenPrintf("Hello PSP HTTPS Browser\n");
-
-    // PSP ネットワーク初期化
+// ネットワーク初期化
+int initNetwork() {
     pspDebugScreenPrintf("Initializing network...\n");
     if(pspNetInit() < 0) {
         pspDebugScreenPrintf("pspNetInit failed.\n");
-        sceKernelSleepThread();
+        return -1;
     }
     if(pspNetInetInit() < 0) {
         pspDebugScreenPrintf("pspNetInetInit failed.\n");
-        sceKernelSleepThread();
+        return -1;
     }
     if(pspNetResolverInit() < 0) {
         pspDebugScreenPrintf("pspNetResolverInit failed.\n");
-        sceKernelSleepThread();
+        return -1;
     }
     pspDebugScreenPrintf("Network initialized.\n");
+    return 0;
+}
 
-    // HTTPSリクエスト実行
-    https_request();
-
-    // 終了待ち
-    pspDebugScreenPrintf("Press any key to exit...\n");
-    sceKernelDelayThread(5000000);
-    
-    // ネットワーク終了処理
+// ネットワーク終了処理
+void termNetwork() {
     pspNetResolverTerm();
     pspNetInetTerm();
     pspNetTerm();
+}
 
+// HTTPS 通信サンプル（example.com へ GET リクエストを送信）
+int https_request() {
+    const char *server_hostname = "example.com";
+    const int server_port = 443;
+    const char *GET_request = "GET / HTTP/1.1\r\n"
+                              "Host: example.com\r\n"
+                              "User-Agent: PSP WolfSSL Browser\r\n"
+                              "Connection: close\r\n\r\n";
+
+    int sock = -1;
+    int ret = 0;
+
+    // wolfSSL 初期化
+    wolfSSL_Init();
+
+    // クライアント用コンテキスト作成（TLSv1.2 クライアント）
+    WOLFSSL_METHOD* method = wolfTLSv1_2_client_method();
+    if (method == NULL) {
+        pspDebugScreenPrintf("wolfTLSv1_2_client_method error\n");
+        return -1;
+    }
+    WOLFSSL_CTX* ctx = wolfSSL_CTX_new(method);
+    if (ctx == NULL) {
+        pspDebugScreenPrintf("wolfSSL_CTX_new error\n");
+        return -1;
+    }
+
+    // ソケット作成
+    sock = sceNetInetSocket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        pspDebugScreenPrintf("sceNetInetSocket error\n");
+        wolfSSL_CTX_free(ctx);
+        return -1;
+    }
+
+    // サーバアドレス設定
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = sceNetHtons(server_port);
+    // example.com の IP アドレス 93.184.216.34 を設定（名前解決を行わない場合）
+    server_addr.sin_addr.s_addr = sceNetInetAddr("93.184.216.34");
+
+    pspDebugScreenPrintf("Connecting to %s:%d...\n", server_hostname, server_port);
+    if (sceNetInetConnect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        pspDebugScreenPrintf("sceNetInetConnect error\n");
+        sceNetInetSocketClose(sock);
+        wolfSSL_CTX_free(ctx);
+        return -1;
+    }
+    pspDebugScreenPrintf("Connected to server.\n");
+
+    // wolfSSL オブジェクト作成し、ソケットを設定
+    WOLFSSL* ssl = wolfSSL_new(ctx);
+    if (ssl == NULL) {
+        pspDebugScreenPrintf("wolfSSL_new error\n");
+        sceNetInetSocketClose(sock);
+        wolfSSL_CTX_free(ctx);
+        return -1;
+    }
+    wolfSSL_set_fd(ssl, sock);
+
+    // SNI 設定
+    ret = wolfSSL_UseSNI(ssl, WOLFSSL_SNI_HOSTNAME, server_hostname, strlen(server_hostname));
+    if (ret != WOLFSSL_SUCCESS) {
+        pspDebugScreenPrintf("wolfSSL_UseSNI error: %d\n", ret);
+        wolfSSL_free(ssl);
+        sceNetInetSocketClose(sock);
+        wolfSSL_CTX_free(ctx);
+        return -1;
+    }
+
+    // TLS ハンドシェイク
+    pspDebugScreenPrintf("Performing TLS handshake...\n");
+    ret = wolfSSL_connect(ssl);
+    if (ret != WOLFSSL_SUCCESS) {
+        int err = wolfSSL_get_error(ssl, ret);
+        pspDebugScreenPrintf("wolfSSL_connect error: %d\n", err);
+        wolfSSL_free(ssl);
+        sceNetInetSocketClose(sock);
+        wolfSSL_CTX_free(ctx);
+        return -1;
+    }
+    pspDebugScreenPrintf("TLS handshake successful!\n");
+
+    // GET リクエスト送信
+    pspDebugScreenPrintf("Sending GET request...\n");
+    ret = wolfSSL_write(ssl, GET_request, strlen(GET_request));
+    if (ret <= 0) {
+        int err = wolfSSL_get_error(ssl, ret);
+        pspDebugScreenPrintf("wolfSSL_write error: %d\n", err);
+        wolfSSL_free(ssl);
+        sceNetInetSocketClose(sock);
+        wolfSSL_CTX_free(ctx);
+        return -1;
+    }
+
+    // レスポンス受信
+    pspDebugScreenPrintf("Receiving response...\n");
+    char buf[1024];
+    int bytes;
+    do {
+        memset(buf, 0, sizeof(buf));
+        bytes = wolfSSL_read(ssl, buf, sizeof(buf) - 1);
+        if (bytes > 0) {
+            buf[bytes] = '\0';
+            pspDebugScreenPrintf("%s", buf);
+        }
+    } while (bytes > 0);
+
+    pspDebugScreenPrintf("\nConnection closed.\n");
+
+    // クリーンアップ
+    wolfSSL_shutdown(ssl);
+    wolfSSL_free(ssl);
+    sceNetInetSocketClose(sock);
+    wolfSSL_CTX_free(ctx);
+    wolfSSL_Cleanup();
+
+    return 0;
+}
+
+extern "C" int main(void) {
+    setupCallbacks();
+    pspDebugScreenInit();
+    pspDebugScreenPrintf("Hello PSP HTTPS Browser (wolfSSL)\n");
+
+    // ネットワーク初期化
+    if (initNetwork() < 0) {
+        pspDebugScreenPrintf("Network initialization failed. Exiting...\n");
+        sceKernelSleepThread();
+    }
+
+    // APCTL（アクセスポイント）接続待ち（IP取得待ち）
+    pspDebugScreenPrintf("Connecting to network access point...\n");
+    {
+        SceNetApctlState state;
+        memset(&state, 0, sizeof(state));
+        while (1) {
+            sceNetApctlGetState(&state);
+            if (state == SCE_NET_APCTL_STATE_GOT_IP) {
+                break;
+            }
+            sceKernelDelayThread(50 * 1000); // 50ms
+        }
+    }
+    pspDebugScreenPrintf("Network access point connected.\n");
+
+    // HTTPS リクエスト実行
+    https_request();
+
+    pspDebugScreenPrintf("Press any key to exit...\n");
+    sceKernelDelayThread(5000000);  // 5秒待機
+
+    termNetwork();
     sceKernelExitGame();
     return 0;
 }
