a04849f80ac50ddbb988018fea9258e713e20d41
japan00x <137403487+japan00x@users.noreply.github.com>
2025-04-05 04:45:46 +0900

    Add files via upload

---
+++ src/test/main.cpp
@@ -0,0 +1,31 @@
+#include <pspkernel.h>
+#include <pspdebug.h>
+
+PSP_MODULE_INFO("Tutorial", 0, 1, 0);
+
+int exit_callback(int arg1, int arg2, void* common) {
+	sceKernelExitGame();
+	return 0;
+}
+
+int callbackThread(SceSize args, void* argp) {
+	int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+	sceKernelRegisterExitCallback(cbid);
+
+	sceKernelSleepThreadCB();
+	return 0;
+}
+
+void setupCallbacks() {
+	int thid = sceKernelCreateThread("update_thread", callbackThread, 0x11, 0xFA0, 0, NULL);
+	if(thid >= 0){
+		sceKernelStartThread(thid, 0, NULL);
+	}
+}
+
+auto main() -> int {
+	setupCallbacks();
+	pspDebugScreenInit();
+	
+	pspDebugScreenPrintf("Hello World!");
+}
